25ce5d78baaa554c5f5fde35a85311e48bb3fb06
Sakura <Sakura@DESKTOP-2JG9OSM>
2023-11-22 21:28:58 +0800

    ViewManager

--- CPF.Toolkit/ViewManager.cs
+++ CPF.Toolkit/ViewManager.cs
@@ -11,9 +11,32 @@ namespace CPF.Toolkit
         public static T View<T>(params object[] arges) where T : Window
         {
             var view = Activator.CreateInstance(typeof(T), arges) as T;
-            view.Initialized += View_Initialized;
             view.Closing += View_Closing;
+            view.PropertyChanged += View_PropertyChanged;
             return view;
+        }
+
+        private static void View_PropertyChanged(object sender, CPFPropertyChangedEventArgs e)
+        {
+            var view = sender as Window;
+            if (e.PropertyName == nameof(Window.DataContext))
+            {
+                if (view.DataContext is IClosable closable)
+                {
+                    closable.Closable += (ss, dialogResult) =>
+                    {
+                        if (view.IsDialogMode == true)
+                        {
+                            view.DialogResult = dialogResult;
+                        }
+                        view.Close();
+                    };
+                }
+                if (view.DataContext is IDialog dialog)
+                {
+                    dialog.Dialog = new DialogService(view);
+                }
+            }
         }
 
         private static void View_Closing(object sender, ClosingEventArgs e)
@@ -22,26 +45,6 @@ namespace CPF.Toolkit
             if (view.DataContext is IClosable closable)
             {
                 closable.OnClosable(sender, e);
-            }
-        }
-
-        private static void View_Initialized(object sender, EventArgs e)
-        {
-            var view = sender as Window;
-            if (view.DataContext is IClosable closable)
-            {
-                closable.Closable += (ss, dialogResult) =>
-                {
-                    if (view.IsDialogMode == true)
-                    {
-                        view.DialogResult = dialogResult;
-                    }
-                    view.Close();
-                };
-            }
-            if (view.DataContext is IDialog dialog)
-            {
-                dialog.Dialog = new DialogService(view);
             }
         }
     }
