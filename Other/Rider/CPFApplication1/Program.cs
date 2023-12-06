fb9b04e7a00d5c46d9d3c39904b335f7a7454ec0
小红帽 <761716178@qq.com>
2023-12-06 13:58:36 +0800

    增加Rider项目模板

---
+++ Other/Rider/CPFApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using CPF.Platform;
+using CPF.Skia;
+using CPF.Windows;
+using System;
+
+namespace CPFApplication1
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.Initialize(
+                (OperatingSystemType.Windows, new WindowsPlatform(), new SkiaDrawingFactory())
+                , (OperatingSystemType.OSX, new CPF.Mac.MacPlatform(), new SkiaDrawingFactory())//如果需要支持Mac才需要
+                , (OperatingSystemType.Linux, new CPF.Linux.LinuxPlatform(), new SkiaDrawingFactory())//如果需要支持Linux才需要
+            );
+            Application.Run(new Window1());
+        }
+    }
+}
---
+++ Other/Rider/CPFApplication1/Window1.cs
@@ -0,0 +1,52 @@
+﻿using CPF;
+using CPF.Animation;
+using CPF.Charts;
+using CPF.Controls;
+using CPF.Drawing;
+using CPF.Shapes;
+using CPF.Styling;
+using CPF.Svg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CPFApplication1
+{
+    public class Window1 : Window
+    {
+        protected override void InitializeComponent()
+        {
+            LoadStyleFile("res://CPFApplication1/Stylesheet1.css");//加载样式文件，文件需要设置为内嵌资源
+
+            Title = "标题";
+            Width = 500;
+            Height = 400;
+            Background = null;
+            Children.Add(new WindowFrame(this, new Panel
+            {
+                Width = "100%",
+                Height = "100%",
+                Children =
+                {
+                    new Button{ Content="按钮" }
+                }
+            }));
+
+            if (!DesignMode)//设计模式下不执行，也可以用#if !DesignMode
+            {
+
+            }
+        }
+
+#if !DesignMode //用户代码写到这里，设计器下不执行，防止设计器出错
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+
+        }
+        //用户代码
+
+#endif
+    }
+}
